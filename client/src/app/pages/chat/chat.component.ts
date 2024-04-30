5fa24fa05cde03f581c673d6356e49ee13a63459
Jose Villalta <josevillalta@vimasistem.com>
2024-04-29 20:08:30 -0500

    Chat

--- client/src/app/pages/chat/chat.component.ts
+++ client/src/app/pages/chat/chat.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+	selector: 'app-chat',
+	templateUrl: './chat.component.html',
+	styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent {
-
+	users = [1, 2, 3, 4, 5];
 }
